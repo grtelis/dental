d0e91aa4c8f7fd33daeaa174da9e09052f895689
grtelis <elizabeth_torres@comunidad.unam.mx>
2023-05-21 13:07:01 -0600

    diseño de la paginación

--- sistema/articulos/articulos.php
+++ sistema/articulos/articulos.php
@@ -12,19 +12,12 @@
   // Calcula el número total de páginas
 $total_paginas = ceil($total / $resultados_por_pagina);
 
-
-
-
-
-
 if (isset($_POST['id'])) {
   $desplazamiento = ($_POST['id'] - 1) * $resultados_por_pagina;
     $arr_articulo = $obj_Articulo->buscarTodos($desplazamiento, $resultados_por_pagina);
 }else{
    $arr_articulo = $obj_Articulo->buscarTodos(0,5);
 }
-
-
 
 ?>
 
@@ -75,80 +68,58 @@ if (isset($_POST['id'])) {
 
      
           </table>
-      </div>
-    </div>  
-  </div>
-
-           <?php
+          <?php
          
          echo '<nav aria-label="Page navigation example">
                 <ul class="pagination">
                   <li class="page-item';
-          if (!isset($_POST['id'])) {
+          if (!isset($_POST['id']) || $_POST['id'] == 1) {
   
             echo  ' disabled';
-                  //   <a class="page-link" href="#" aria-label="Previous">
-                  //     <span aria-hidden="true">&laquo;</span>
-                  //   </a>
-                  // </li>
-                  // <li class="page-item active">
-                  //   <a class="page-link">' . 1 . '</a>
-                  // </li>';
   
           }
 
+          $anterior = !isset($_POST['id']) ? 1 : $_POST['id'] - 1;
+
           echo '">
-                  <a class="page-link" href="#" aria-label="Previous">
+                  <a class="page-link" role="button" onclick="consultarArticulos(' . $anterior . ')" aria-label="Previous">
                     <span aria-hidden="true">&laquo;</span>
                   </a>
                 </li>';
 
          for ($i = 1; $i <= $total_paginas; $i++) {
           
-          echo '"<li id="'.$i.'" class="page-item';
+          echo '<li id="'.$i.'" class="page-item';
 
-          if (!isset($_POST['id']) && $i == 1) {
+          if ((!isset($_POST['id']) && $i == 1) || $i == $_POST['id']) {
 
-            
-  
             echo ' active';
 
-         } else if ($i == $_POST['id']) {
-          
-            echo ' active';
+         } 
 
-         }
-
-         echo '"><a class="page-link" onclick="consultarArticulos(' . $i . ')">' . $i . '</a></li>';
+         echo '"><a class="page-link" role="button" onclick="consultarArticulos(' . $i . ')">' . $i . '</a></li>';
 
         }
 
-        echo '<li class="page-item">
-          <a class="page-link" href="#" aria-label="Next">
-            <span aria-hidden="true">&raquo;</span>
-          </a>
-        </li>
-        </ul></nav>';
+        echo '<li class="page-item';
+        
+        
+        if ($total_paginas == 1 || $_POST['id'] == $total_paginas) {
 
-         
-       
+          echo ' disabled';
+        }
+        
+        $siguiente = !isset($_POST['id']) ? 2 : $_POST['id'] + 1;
 
-          // for ($i = 1; $i <= $total_paginas; $i++) {
-          //   echo '<li class="page-item';
-          //   if ($i == $_POST['id']) {
-          //     echo ' active';
-          //   }
-          //   echo '"><a class="page-link" onclick="consultarArticulos(' . $i . ')">' . $i . '</a></li>';
-          // }
-        //   echo '<li class="page-item">
-        //   <a class="page-link" href="#" aria-label="Next">
-        //     <span aria-hidden="true">&raquo;</span>
-        //   </a>
-        // </li>';
-
-          // echo '';
-
-
-?>
+        echo '">
+        <a class="page-link" role="button" onclick="consultarArticulos(' . $siguiente . ')" aria-label="Next">
+          <span aria-hidden="true">&raquo;</span>
+        </a>
+      </li>
+      </ul></nav>';
+  ?>
+      </div>
+    </div>  
+  </div>
 </section>
 <script src="../sistema/articulos/articulos.js"></script>
